da854deed1e82c5fc4dc05e418e9548cbce40698
AKHILESH PG <102977574+akhilesh6699@users.noreply.github.com>
2023-09-07 12:25:49 +0530

    #login

--- server.js
+++ server.js
@@ -1,36 +1,22 @@
 const express = require("express");
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
+const morgan = require("morgan");
+require("../node/db/connect");
+const bodyParser = require("body-parser");
 
-const EmployeeRoute = require('./routes/employee')
+const EmployeeRoute = require("./routes/employee");
 const AuthRoute = require("./routes/auth");
 
-mongoose.connect("mongodb://127.0.0.1/asset", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-const db = mongoose.connection
-
-db.on('error', (err) =>{
-    console.log(err)
-})
-
-db.once('open', () =>{
-    console.log('Database Connection Established!')
-})
-
-const app = express()
-
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`)
-})
 
 app.use("/api", AuthRoute);
-app.use('/api/employee', EmployeeRoute)
+app.use("/api/employee", EmployeeRoute);
